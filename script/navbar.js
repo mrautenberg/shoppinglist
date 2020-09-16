9ba30fff668ec7886aa6d9ee6e811293c01bb29b
mrautenberg <mrautenberg@outlook.com>
2020-09-16 16:07:54 +0200

    color theme update + slider animation bug fix

---
+++ script/navbar.js
@@ -0,0 +1,24 @@
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+  // Toggle Nav
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+    // Animate Links
+    // Index used to animate delay between each listitem
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 1 ease forwards ${index / 7 + 1.5}s`;
+      }
+    });
+    // Burger animation
+    burger.classList.toggle('toggle');
+  });
+};
+
+navSlide();
