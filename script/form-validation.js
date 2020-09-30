08f70e55cc922711d71adf6433c0de479578168b
mrautenberg <mrautenberg@outlook.com>
2020-09-30 11:09:12 +0200

    redirected after form submission issue fixed

--- script/form-validation.js
+++ script/form-validation.js
@@ -7,6 +7,7 @@ const messege = document.querySelector("#messege");
 // FUNCTIONS
 // err mssg for input
 const showError = (input, messege) => {
+    // todo: ta bort className, använd istället classList.add / classList.remove
     const formControl = input.parentElement;
     formControl.className = "form__control error";
 
@@ -17,6 +18,7 @@ const showError = (input, messege) => {
 
 // show success outline
 const showSuccess = (input) => {
+    // samma todo som ovan
     const formControl = input.parentElement;
     formControl.className = "form__control success";
 };
@@ -65,11 +67,30 @@ const getFieldName = (input) => {
 };
 
 // EVENT LISTENERS
-const submit = form.addEventListener("submit", (e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault(e);
 
     checkRequired([name, email, messege]);
     checkLength(name, 3, 20);
     checkLength(messege, 3, 400);
     checkEmail(email);
+
+    const success = document.querySelectorAll(".success").length;
+    const error = document.querySelectorAll(".error").length;
+
+    if(success === 3) {
+        window.location.href = "./form-submitted.html";
+        console.log('WOHOW! 10 poäng!')
+    }
+
+    //console.log(success);
+    //console.log(error);
+
+   for (i= 0; i < success.length; i++) {
+       console.log('success!')
+   }
+
+
+    // om inga fel föreligger - redirecta vidare (location.href)
+    // document.querySelectorAll(?).length
 });
--- script/no-window-resize.js
+++ script/no-window-resize.js
@@ -2,9 +2,9 @@
 let resizeTimer;
 
 window.addEventListener("resize", () => {
-  document.body.classList.add("resize-animation-stopper");
-  clearTimeout(resizeTimer);
-  resizeTimer = setTimeout(() => {
-    document.body.classList.remove("resize-animation-stopper");
-  }, 400);
+    document.body.classList.add("resize-animation-stopper");
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove("resize-animation-stopper");
+    }, 400);
 });
